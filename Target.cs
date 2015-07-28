4060bf3535a70a455a7649088c89419d2ee613c3
Guilherme Thomazi Bonicontro <thomazi@linux.com>
2015-07-28 10:55:43 -0300

    Create Target.cs

---
+++ Target.cs
@@ -0,0 +1,13 @@
+using System;
+
+namespace Target
+{
+    class Program
+    {
+        public static void Main(string[] args)
+        {
+            Console.WriteLine("DUMMY APP HERE YO!");
+            Console.ReadLine();
+        }
+    }
+}
